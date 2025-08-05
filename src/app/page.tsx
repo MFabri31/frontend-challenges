a5da3c87493e3468aeffc5ea95e98066635eaf4a
mfabri31 <fabriciomedina9531@gmail.com>
2025-08-05 17:56:42 -0300

    Feat: add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Footer from "./components/Footer";
 import Navbar from "./components/Nav";
 
 export default function Home() {
   return (
     <div className="font-sans min-h-screen">
       <Navbar />
+      <Footer />
     </div>
   );
 }
